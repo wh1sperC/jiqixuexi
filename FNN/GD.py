bff88ea88e1e96b2e4ca59aa1a8ebd64d8beb33f
wh1sperC <1052046259@qq.com>
2021-12-15 23:30:23 +0800

    update

---
+++ FNN/GD.py
@@ -0,0 +1,21 @@
+# 使用梯度下降实现FNN神经网络
+import numpy as np
+import matplotlib.pyplot as plt
+import chineseize_matplotlib
+from numpy.lib.npyio import load
+
+
+# 加载数据
+def load_data(path,m):
+    data=np.loadtxt(path,dtype=float,ndmin=m)
+    return data
+
+x_path1='./FNN/Exam/train/x.txt'
+x_path2='./FNN/Iris/train/x.txt'
+y_path1='./FNN/Exam/train/y.txt'
+y_path2='./FNN/Iris/train/y.txt'
+data1=load_data(x_path1,2)
+data2=load_data(x_path2,2)
+label1=load_data(y_path1,1)
+label2=load_data(y_path2,1)
+print(data1)
