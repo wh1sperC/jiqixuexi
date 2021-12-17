cade4154b10535fb6e1a5d69e6b3d51af1bfe891
wh1sperC <1052046259@qq.com>
2021-12-18 00:10:53 +0800

    updat

--- FNN/GD.py
+++ FNN/GD.py
@@ -10,25 +10,16 @@ def load_data(path,m):
     data=np.loadtxt(path,dtype=float,ndmin=m)
     return data
 
-x_path1='./FNN/Exam/train/x.txt'
-x_path2='./FNN/Iris/train/x.txt'
-y_path1='./FNN/Exam/train/y.txt'
-y_path2='./FNN/Iris/train/y.txt'
-data1=load_data(x_path1,2)
-data2=load_data(x_path2,2)
-label1=load_data(y_path1,2)
-label2=load_data(y_path2,2)
-print(data1)
-print(label1)
-
 # 定义神经网络的组成
 class FNN:
-    #初始化网络结构，其中sizes是[输入层，任意多隐藏层，输出层]的节点数的列表，f是我们需要训练的数据的特征数
-    def __init__(self,sizes): 
+    #初始化网络结构
+    # 其中sizes是[输入层，任意多隐藏层，输出层]的节点数的列表，f是我们需要训练的数据的特征数
+    #f是数据维度,f=2代表输入数据是二维的
+    def __init__(self,sizes,f): 
         self.layer_nums=len(sizes) #获取网络层数
         self.sizes=sizes
-        self.bayes=[np.random.randn(i,1) for i in sizes[1:]] #获取每层贝叶斯偏置
-        self.weights=[np.random.randn(i,j) for i,j in zip(sizes[1:],sizes[:-1])] #获取每一层的权重
+        self.bayes=[np.random.rand(i,1) for i in sizes[1:]] #获取每层贝叶斯偏置
+        self.weights=[np.random.rand(i,j,f) for i,j in zip(sizes[1:],sizes[:-1])] #获取每一层的权重
     
     # 定义激活函数，这里使用sigmoid
     def sigmoid(self,x):
@@ -40,8 +31,19 @@ class FNN:
             pass
 
 if __name__ == '__main__':
-    net=FNN([3,2,1])
+    '''x_path1='./FNN/Exam/train/x.txt'
+    x_path2='./FNN/Iris/train/x.txt'
+    y_path1='./FNN/Exam/train/y.txt'
+    y_path2='./FNN/Iris/train/y.txt'
+    data1=load_data(x_path1,2)
+    data2=load_data(x_path2,2)
+    label1=load_data(y_path1,2)
+    label2=load_data(y_path2,2)
+    print(data1)
+    print(label1)'''
+    net=FNN([3,2,1],2)
     print(net.layer_nums)
     print(net.sizes)
     print(net.bayes)
     print(net.weights)
+    #print(np.random.randn(1,2,2))
