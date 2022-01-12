ff9b62f58f0f579a2619d556671a5a49240f4e0a
wh1sperC <1052046259@qq.com>
2022-01-12 15:09:41 +0800

    update

--- svm/exam.py
+++ svm/exam.py
@@ -8,10 +8,10 @@ x1 = np.loadtxt("./svm/Exam/test/x.txt")
 y1 = np.loadtxt("./svm/Exam/test/y.txt")
 
 # 训练svm分类器
-classifiers = svm.SVC(C=2, kernel='linear', gamma=100, decision_function_shape='ovr')  # 各项参数
+classifiers = svm.SVC(C=2, kernel='linear', gamma=100, decision_function_shape='ovo')  # 各项参数
 classifiers.fit(x, y.ravel())
 
-# 打印结果
+# 打印结
 print("训练集：", classifiers.score(x, y))
 print("测试集: ", classifiers.score(x1, y1))
 print('train_decision_function:\n', classifiers.decision_function(x))
