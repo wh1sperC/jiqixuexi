a3022a651dcbbae8a1b72a1d91610a1209c854d3
wh1sperC <1052046259@qq.com>
2022-01-30 23:47:27 +0800

    update

--- softmax/GD.py
+++ softmax/GD.py
@@ -21,33 +21,37 @@ def calError(h, label, type):
     return ans
 
 def loadTrainSet():
-    with open('./softmax/Iris/train/x.txt') as f:
-        data = []
-        for line in f:
-            lineArr = line.strip().split()
-            data.append([float(lineArr[0]), float(lineArr[1])])
-    with open('./softmax/Iris/train/y.txt') as f:
-        label = []
-        for line in f:
-            lineArr = line.strip().split()
-            label.append(int(float(lineArr[0])))
+    f = open('./softmax/Iris/train/x.txt')
+    data = []
+    for line in f:
+        lineArr = line.strip().split()
+        data.append([float(lineArr[0]), float(lineArr[1])])
+    f.close()
+    f = open('./softmax/Iris/train/y.txt')
+    label = []
+    for line in f:
+        lineArr = line.strip().split()
+        label.append(int(float(lineArr[0])))
+    f.close()
     data = normalization(data)
     data1 = []
     for i in data:
         data1.append([1, i[0], i[1]])
     return data1, label
 
-def loadTestSet(): 
-    with open('./softmax/Iris/test/x.txt') as f:
-        data = []
-        for line in f:
-            lineArr = line.strip().split()
-            data.append([float(lineArr[0]), float(lineArr[1])])
-    with open('./softmax/Iris/test/y.txt') as f:
-        label = []
-        for line in f:
-            lineArr = line.strip().split()
-            label.append(int(float(lineArr[0])))
+def loadTestSet():
+    f = open('./softmax/Iris/test/x.txt')
+    data = []
+    for line in f:
+        lineArr = line.strip().split()
+        data.append([float(lineArr[0]), float(lineArr[1])])
+    f.close()
+    f = open('./softmax/Iris/test/y.txt')
+    label = []
+    for line in f:
+        lineArr = line.strip().split()
+        label.append(int(float(lineArr[0])))
+    f.close()
     data = normalization(data)
     data1 = []
     for i in data:
@@ -73,13 +77,33 @@ def calLikelihood(h, labelMat):
         ans = ans + log(h[i, labelMat[i, 0]])
     return ans
 
-         
+def softmaxRegression(data, label):
+    dataMat = mat(data)
+    labelMat = mat(label).transpose()
+    n, m = shape(dataMat)   # n samples, m features
+    theta = zeros((m, 4))   # m features, 3 tpyes + 1
+    alpha = 0.001
+    maxCycle = 10000
+    episilon = 0.0005
+    preLikelihood = 0.0
+    for k in range(maxCycle):
+        h = softmax(dataMat * theta)
+        likelihood = calLikelihood(h, labelMat)
+        if abs(likelihood - preLikelihood) < episilon:
+            break
+        preLikelihood = likelihood
+        for i in range(shape(h)[1]):
+            delta = alpha * dataMat.transpose() * calError(h, labelMat, i)
+
+            theta[:, i] = theta[:, i] + delta.transpose()
+    print(k)
+    return theta         
 
 def stocSoftmaxRegression(data, label):
     dataMat = mat(data)
     labelMat = mat(label).transpose()
-    n, m = shape(dataMat)
-    theta = zeros((m, 4))
+    n, m = shape(dataMat)   # n samples, m features
+    theta = zeros((m, 4))   # m features, 3 tpyes + 1
     alpha = 0.001
     maxCycle = 50000
     episilon = 1e-7
@@ -90,6 +114,7 @@ def stocSoftmaxRegression(data, label):
         if abs(likelihood - preLikelihood) < episilon:
             break
         preLikelihood = likelihood
+        # choose one sample only
         rand = random.randint(0, n - 1)
         for i in range(shape(h)[1]):
             if labelMat[rand, 0] == i:
@@ -120,13 +145,13 @@ def plotBestFit(fig, data, label, theta, name, subplot):
     ax = fig.add_subplot(subplot)
     ax.set_title(name, fontsize=8)
     
-    ax.scatter(xcord0, ycord0, s = 30, c = 'green')
-    ax.scatter(xcord1, ycord1, s = 30, c = 'blue')
-    ax.scatter(xcord2, ycord2, s = 30, c = 'red')
+    ax.scatter(xcord0, ycord0, s = 30, c = 'red')
+    ax.scatter(xcord1, ycord1, s = 30, c = 'green')
+    ax.scatter(xcord2, ycord2, s = 30, c = 'blue')
 
-    plotBoundary(theta[0, 0] - theta[0, 1], theta[1, 0] - theta[1, 1], theta[2,0] - theta[2, 1], "red-blue")
-    plotBoundary(theta[0, 0] - theta[0, 2], theta[1, 0] - theta[1, 2], theta[2,0] - theta[2, 2], "green-blue")
-    plotBoundary(theta[0, 1] - theta[0, 2], theta[1, 1] - theta[1, 2], theta[2,1] - theta[2, 2], "red-green")
+    plotBoundary(theta[0, 0] - theta[0, 1], theta[1, 0] - theta[1, 1], theta[2,0] - theta[2, 1], "red-green")
+    plotBoundary(theta[0, 0] - theta[0, 2], theta[1, 0] - theta[1, 2], theta[2,0] - theta[2, 2], "red-blue")
+    plotBoundary(theta[0, 1] - theta[0, 2], theta[1, 1] - theta[1, 2], theta[2,1] - theta[2, 2], "green-blue")
 
     
 
@@ -137,11 +162,27 @@ def plotBoundary(para0, para1, para2, name):
     plt.legend()
     
 
-if __name__=='__main__':
-    fig = plt.figure(figsize=[5*2,5*1])
+def main():
+    fig = plt.figure()
     data1, label = loadTrainSet()
     testdata, testlabel = loadTestSet()
-    
+    print("SoftmaxRegression:")
+    print("theta:")
+    theta1 = softmaxRegression(data1, label)
+    print(theta1)
+
+    print("to TestDataSet:")    
+    predictlabel1 = getTestPredict(theta1, testdata)
+    print("accuracy:")
+    print(getCorrectRate(predictlabel1, mat(testlabel).transpose()))
+    plotBestFit(fig, testdata, testlabel, theta1, "Softmax, ToTestDataSet", 221)
+
+    print("to TrainDataSet:")    
+    predictlabel2 = getTestPredict(theta1, data1)
+    print("accuracy:")
+    print(getCorrectRate(predictlabel2, mat(label).transpose()))
+    plotBestFit(fig, data1, label, theta1, "Softmax, ToTrainDataSet", 222)
+
     print("stocSoftmaxRegression:")
     print("theta")
     theta2 = stocSoftmaxRegression(data1, label)
@@ -151,13 +192,16 @@ if __name__=='__main__':
     predictlabel1 = getTestPredict(theta2, testdata)
     print("accuracy:")
     print(getCorrectRate(predictlabel1, mat(testlabel).transpose()))
-    plotBestFit(fig, testdata, testlabel, theta2, "stocSoftmax, ToTestDataSet", 122)
+    plotBestFit(fig, testdata, testlabel, theta2, "stocSoftmax, ToTestDataSet", 223)
 
     print("to TrainDataSet:")    
     predictlabel2 = getTestPredict(theta2, data1)
     print("accuracy:")
     print(getCorrectRate(predictlabel2, mat(label).transpose()))
-    plotBestFit(fig, data1, label, theta2, "stocSoftmax, toTrainDataSet", 121)
+    plotBestFit(fig, data1, label, theta2, "stocSoftmax, toTrainDataSet", 224)
 
     plt.show()
+
+if __name__=='__main__':
+    main()
 
